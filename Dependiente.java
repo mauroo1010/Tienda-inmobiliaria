890cac0a6c89bf3752b2f8e1c54df50501bcb161
Mauro <maurogutierreztormo@gmail.com>
2023-04-20 12:30:31 +0200

    Primer commit

---
+++ Dependiente.java
@@ -0,0 +1,17 @@
+/**
+ * Write a description of class dependiente here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dependiente
+{
+    private int edad;
+    
+    private String nombre;
+    
+    public Dependiente(int edad, String nombre){
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+}
