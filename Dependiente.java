40a38dcc6091f0ca55cda843f227a14ec5501f20
Mario <mmerayo1803@gmail.com>
2023-04-20 12:33:24 +0200

    Segundo commit

--- Dependiente.java
+++ Dependiente.java
@@ -14,4 +14,21 @@ public class Dependiente
         this.edad = edad;
         this.nombre = nombre;
     }
+    
+     public int getEdad() {
+        return edad;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
 }
