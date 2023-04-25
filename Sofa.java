cfbfdf9392140dddc83aa489e8a44820989e2084
Mario <mmerayo1803@gmail.com>
2023-04-25 12:13:15 +0200

    Sexto commit

--- Sofa.java
+++ Sofa.java
@@ -10,6 +10,29 @@ public class Sofa{
         this.materialSofa = materialSofa;
         this.vendedorSofa = vendedorSofa;
     }
-        
+    
+    public String getMarca() {
+        return marcaSofa;
+    }
+    
+    public String getMaterial() {
+        return materialSofa;
+    }
+    
+    public String getvendedor() {
+        return vendedorSofa;
+    }
+    
+    public void setMarca(String marcaSofa) {
+         this.marcaSofa=marcaSofa;
+    }
+    
+    public void setMaterial(String materialSofa) {
+         this.materialSofa=materialSofa;
+    }
+    
+    public void setVendedor(String vendedorSofa) {
+         this.vendedorSofa=vendedorSofa;
+    }
     }
 
